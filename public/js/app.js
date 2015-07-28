85f643e13b31bb6511298758faa724eebd33057a
Chris Holt <christopher.d.holt@gmail.com>
2015-07-28 12:41:52 -0700

    adjusted js for Age Ver. Added IIFE, local storage, only runs ageVer if not already verified

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,21 @@
+(function() {
+if (!localStorage['ageCheck']) {
+       localStorage['ageCheck'] = 'yes';
+       checkAge();
+   }
+})();
+
+function checkAge() {
+var ageVer = document.getElementById('ageVerWrap');
+ageVer.className = 'showAge';
 
 var yes = document.getElementById('confirmY');
 yes.addEventListener('click', function(){
-  ageVer = document.getElementById('ageVerWrap');
-  ageVer.className = 'hide'; //hides the ageVer section if they say they are above 21
+  ageVer.className = 'hideAge'; //hides the ageVer section if they say they are above 21
 });
 
 var no = document.getElementById('confirmNot');
 no.addEventListener('click', function(){
   window.open("http://www.disney.com","_self"); //opens disney.com if they say they are not 21
 });
-
+}
