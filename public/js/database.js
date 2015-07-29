80e5c560c5bab467075ede253df511d12821f059
Chris Holt <christopher.d.holt@gmail.com>
2015-07-29 14:13:52 -0700

    event listener for buttons on random beer page.

--- public/js/database.js
+++ public/js/database.js
@@ -6,194 +6,185 @@ $(document).ready(function() {
 
 
 //MongoDB request information
-var db = 'https://api.mongolab.com/api/1/databases/beer-seattle/collections';
-var collection;
-var apiKey = '?apiKey=RjwSNykUJAE_wUTwNZhqr-h0pqxjJzne';
-var contentType = 'application/json';
-var dataType = 'json';
+  var db = 'https://api.mongolab.com/api/1/databases/beer-seattle/collections';
+  var collection;
+  var apiKey = '?apiKey=RjwSNykUJAE_wUTwNZhqr-h0pqxjJzne';
+  var contentType = 'application/json';
+  var dataType = 'json';
 
-//Arrays to store each style of beer
-var ipa,
-    strongAle,
-    stoutPorter,
-    lagerPilsner,
-    scotch,
-    pale,
-    wheat,
-    belgian,
-    sour,
-    bock,
-    misc;
+  //Arrays to store each style of beer
+  var ipa,
+      strongAle,
+      stoutPorter,
+      lagerPilsner,
+      scotch,
+      pale,
+      wheat,
+      belgian,
+      sour,
+      bock,
+      misc;
 
-//MongoDB oid's for each style document in the db
-var ipaId = "/ipa/55b84eb37e1ee7f743ec5671";
-var strongAleId = "/strong-ale/55b8540a7e1ee7f743ec5672";
-var stoutPorterId = "/stout-porter/55b8566c7e1ee7f743ec5673";
-var lagerPilsnerId = "/lager-pilsner/55b8595a7e1ee7f743ec5674";
-var scotchId = "/scotch/55b859cb7e1ee7f743ec5675";
-var paleId = "/pale/55b85c827e1ee7f743ec5676";
-var wheatId = "/wheat/55b85d827e1ee7f743ec5677";
-var belgianId = "/belgian/55b85e8d7e1ee7f743ec5678";
-var sourId = "/sours/55b84ab47e1ee7f743ec5670";
-var bockId = "/bock/55b85ee67e1ee7f743ec5679";
-var miscId = "/misc/55b85fb57e1ee7f743ec567a";
+  //MongoDB oid's for each style document in the db
+  var ipaId = "/ipa/55b84eb37e1ee7f743ec5671";
+  var strongAleId = "/strong-ale/55b8540a7e1ee7f743ec5672";
+  var stoutPorterId = "/stout-porter/55b8566c7e1ee7f743ec5673";
+  var lagerPilsnerId = "/lager-pilsner/55b8595a7e1ee7f743ec5674";
+  var scotchId = "/scotch/55b859cb7e1ee7f743ec5675";
+  var paleId = "/pale/55b85c827e1ee7f743ec5676";
+  var wheatId = "/wheat/55b85d827e1ee7f743ec5677";
+  var belgianId = "/belgian/55b85e8d7e1ee7f743ec5678";
+  var sourId = "/sours/55b84ab47e1ee7f743ec5670";
+  var bockId = "/bock/55b85ee67e1ee7f743ec5679";
+  var miscId = "/misc/55b85fb57e1ee7f743ec567a";
 
-//Populate a style array with data from MongoDB
-var requestBeersByStyle = function(style) {
-  switch(style) {
-    case "ipa":
-      if(ipa) {
-        return;
-      } else {
-        collection = ipaId;
-      }
-      break;
-    case "strongAle":
-      if(strongAle) {
-        return;
-      } else {
-      collection = strongAleId;
-      }
-      break;
-    case "stoutPorter":
-      if(stoutPorter) {
-        return;
-      } else {
-        collection = stoutPorterId;
-      }
-      break;
-    case "lagerPilsner":
-      if(lagerPilsner) {
-        return;
-      } else {
-      collection = lagerPilsnerId;
-      }
-      break;
-    case "scotch":
-      if(scotch) {
-        return;
-      } else {
-        collection = scotchId;
-      }
-      break;
-    case "pale":
-      if(pale) {
-        return;
-      } else {
-        collection = paleId;
-      }
-      break;
-    case "wheat":
-      if(wheat) {
-        return;
-      } else {
-        collection = wheatId;
-      }
-      break;
-    case "belgian":
-      if(belgian) {
-        return;
-      } else {
-        collection = belgianId;
-      }
-      break;
-    case "sour":
-      if(sour) {
-        return;
-      } else {
-        collection = sourId;
-      }
-      break;
-    case "bock":
-      if(bock) {
-        return;
-      } else {
-        collection = bockId;
-      }
-      break;
-    case "misc":
-      if(misc) {
-        return;
-      } else {
-      collection = miscId;
-    }
-      break;
-  }
-  $.ajax({
-    url: db + collection + apiKey,
-    type:'GET',
-    async: true,
-    contentType: contentType,
-    dataType: dataType,
-    success: console.log("Connected to MongoDB")
-  })
-  .done(function(response) {
-    // console.log("in done");
+  //Populate a style array with data from MongoDB
+  var requestBeersByStyle = function(style) {
     switch(style) {
       case "ipa":
-        // console.log("ipa bitches!");
-        ipa = response.data;
-        // console.log(ipa);
+        if(ipa) {
+          return;
+        } else {
+          collection = ipaId;
+        }
         break;
       case "strongAle":
-        strongAle = response.data;
+        if(strongAle) {
+          return;
+        } else {
+        collection = strongAleId;
+        }
         break;
       case "stoutPorter":
-        stoutPorter = response.data;
+        if(stoutPorter) {
+          return;
+        } else {
+          collection = stoutPorterId;
+        }
         break;
       case "lagerPilsner":
-        lagerPilsner = response.data;
+        if(lagerPilsner) {
+          return;
+        } else {
+        collection = lagerPilsnerId;
+        }
         break;
       case "scotch":
-        scotch = response.data;
+        if(scotch) {
+          return;
+        } else {
+          collection = scotchId;
+        }
         break;
       case "pale":
-        pale = response.data;
+        if(pale) {
+          return;
+        } else {
+          collection = paleId;
+        }
         break;
       case "wheat":
-        wheat = response.data;
+        if(wheat) {
+          return;
+        } else {
+          collection = wheatId;
+        }
         break;
       case "belgian":
-        belgian = response.data;
+        if(belgian) {
+          return;
+        } else {
+          collection = belgianId;
+        }
         break;
       case "sour":
-        sour = response.data;
+        if(sour) {
+          return;
+        } else {
+          collection = sourId;
+        }
         break;
       case "bock":
-        bock = response.data;
+        if(bock) {
+          return;
+        } else {
+          collection = bockId;
+        }
         break;
       case "misc":
-        misc = response.data;
+        if(misc) {
+          return;
+        } else {
+        collection = miscId;
+      }
         break;
     }
-    $(document).ajaxStop(function () {
-      $("#loading").hide();
-    });
-  })
-  .fail(function(error) {
-  console.log(error);
-});
-}
 
+    $.ajax({
+      url: db + collection + apiKey,
+      type:'GET',
+      async: true,
+      contentType: contentType,
+      dataType: dataType,
+      success: console.log("Connected to MongoDB")
+    })
+    .done(function(response) {
+      // console.log("in done");
+      switch(style) {
+        case "ipa":
+          // console.log("ipa bitches!");
+          ipa = response.data;
+          // console.log(ipa);
+          break;
+        case "strongAle":
+          strongAle = response.data;
+          break;
+        case "stoutPorter":
+          stoutPorter = response.data;
+          break;
+        case "lagerPilsner":
+          lagerPilsner = response.data;
+          break;
+        case "scotch":
+          scotch = response.data;
+          break;
+        case "pale":
+          pale = response.data;
+          break;
+        case "wheat":
+          wheat = response.data;
+          break;
+        case "belgian":
+          belgian = response.data;
+          break;
+        case "sour":
+          sour = response.data;
+          break;
+        case "bock":
+          bock = response.data;
+          break;
+        case "misc":
+          misc = response.data;
+          break;
+      }
+      $(document).ajaxStop(function () {
+        $("#loading").hide();
+      });
+    })
+    .fail(function(error) {
+    console.log(error);
+  });
+};
 
+// var triedHistoryArray = [];  // THIS HAS TO BE THE BEERS ACUTALLY TRIED, NOT JUST SUGGESTED!
 
-
-
-// })
-
-
-
-var triedHistoryArray = [];  // THIS HAS TO BE THE BEERS ACUTALLY TRIED, NOT JUST SUGGESTED!
-
-var totalHistoryArray = [] // this includes suggested beers that have not yet been tried IN ADDITION to those in triedHistoryArray
+// var totalHistoryArray = [] // this includes suggested beers that have not yet been tried IN ADDITION to those in triedHistoryArray
 
 // requestBeersByStyle("ipa");
 var getRandomBeerByStyle = function(style){
 
+// requestBeersByStyle(style);
 
-requestBeersByStyle(style);
-
-setTimeout(function() {
   var workingArray;
     switch(style) {
       case "ipa":
@@ -232,41 +223,30 @@ setTimeout(function() {
         workingArray = misc;
         break;
     }
+  console.log(workingArray);
 
   var randomBeerIndex = Math.floor(Math.random() * (workingArray.length));
-  totalHistoryArray.push(workingArray[randomBeerIndex])
+  // totalHistoryArray.push(workingArray[randomBeerIndex]);
 
-  //THE LINE BELOW IS JUST HERE FOR TESTING... IT SHOULD NOT BE IN THE PERMANENT CODE!!!!
-    triedHistoryArray.push(workingArray[randomBeerIndex]) // THE LINE ABOVE IS JUST FOR TESTING... IT SHOULD NOT BE IN THE PERMANENT CODE
- // puts beer in history of beers that have been suggested or tried
-  console.log("first random beer:");
-  console.log(workingArray[randomBeerIndex])
+return workingArray[randomBeerIndex];
 
-console.log("total history before suggestion:");
-console.log(totalHistoryArray);
-
-console.log("tried history array:")
-console.log(triedHistoryArray);
-
- }, 7000);
 };
 
-getRandomBeerByStyle("ipa");
-getRandomBeerByStyle('stoutPorter');
-getRandomBeerByStyle('scotch');
-getRandomBeerByStyle('sour');
-// getRandomBeer();  // needs to be linked to an event listener\
-
-
-// setTimeout(function() { suggestSimilar(ipa); }, 16000);
-
-
-// suggestSimilar();
+//Event Listener randomBeer.html buttons//
+  $('.randBtn').on('click', function(e){
+    requestBeersByStyle(e.target.id);
+    setTimeout(function() {
+    var beer = getRandomBeerByStyle(e.target.id);
+    // window.open('beer.html', '_self');
+    // $('')
+    console.log(beer);
+    }, 7000);
+  });
 
 function suggestSimilar(style) {
   // if no beers have been TRIED yet, you need to do that first!
   if (!triedHistoryArray)  {
-    console.log("nothing tried yet!")
+    console.log("nothing tried yet!");
     alert("you need to try something before we can make any suggestions based on what you've tried!");
     return;
   }
---
+++ public/js/listeners.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+
+//   $('.randBtn').on('click', function(e){
+//     var beer = getRandomBeerByStyle(e.target.id);
+//     console.log(beer);
+//   });
+
+// });
