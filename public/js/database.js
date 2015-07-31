1a62ec5e1e0a5743d0411d60556c87eb9b2af8a5
Craig Campbell <craig.campbell8@gmail.com>
2015-07-30 20:38:26 -0700

    linted mostly

--- public/js/database.js
+++ public/js/database.js
@@ -1,6 +1,5 @@
+'use strict';
 $(document).ready(function() {
-
-
 //MongoDB request information
   var db = 'https://api.mongolab.com/api/1/databases/beer-seattle/collections';
   var collection;
@@ -24,93 +23,93 @@ $(document).ready(function() {
 var beerHistory = [];
 
   //MongoDB oid's for each style document in the db
-  var ipaId = "/ipa/55b84eb37e1ee7f743ec5671";
-  var strongAleId = "/strong-ale/55b8540a7e1ee7f743ec5672";
-  var stoutPorterId = "/stout-porter/55b8566c7e1ee7f743ec5673";
-  var lagerPilsnerId = "/lager-pilsner/55b8595a7e1ee7f743ec5674";
-  var scotchId = "/scotch/55b859cb7e1ee7f743ec5675";
-  var paleId = "/pale/55b85c827e1ee7f743ec5676";
-  var wheatId = "/wheat/55b85d827e1ee7f743ec5677";
-  var belgianId = "/belgian/55b85e8d7e1ee7f743ec5678";
-  var sourId = "/sours/55b84ab47e1ee7f743ec5670";
-  var bockId = "/bock/55b85ee67e1ee7f743ec5679";
-  var miscId = "/misc/55b85fb57e1ee7f743ec567a";
+  var ipaId = '/ipa/55b84eb37e1ee7f743ec5671';
+  var strongAleId = '/strong-ale/55b8540a7e1ee7f743ec5672';
+  var stoutPorterId = '/stout-porter/55b8566c7e1ee7f743ec5673';
+  var lagerPilsnerId = '/lager-pilsner/55b8595a7e1ee7f743ec5674';
+  var scotchId = '/scotch/55b859cb7e1ee7f743ec5675';
+  var paleId = '/pale/55b85c827e1ee7f743ec5676';
+  var wheatId = '/wheat/55b85d827e1ee7f743ec5677';
+  var belgianId = '/belgian/55b85e8d7e1ee7f743ec5678';
+  var sourId = '/sours/55b84ab47e1ee7f743ec5670';
+  var bockId = '/bock/55b85ee67e1ee7f743ec5679';
+  var miscId = '/misc/55b85fb57e1ee7f743ec567a';
 
   //Populate a style array with data from MongoDB
   var requestBeersByStyle = function(style) {
 
   switch(style) {
-    case "ipa":
+    case 'ipa':
       if(ipa) {
         return;
       } else {
         collection = ipaId;
       }
       break;
-    case "strongAle":
+    case 'strongAle':
       if(strongAle) {
         return;
       } else {
       collection = strongAleId;
       }
       break;
-    case "stoutPorter":
+    case 'stoutPorter':
       if(stoutPorter) {
         return;
       } else {
         collection = stoutPorterId;
       }
       break;
-    case "lagerPilsner":
+    case 'lagerPilsner':
       if(lagerPilsner) {
         return;
       } else {
       collection = lagerPilsnerId;
       }
       break;
-    case "scotch":
+    case 'scotch':
       if(scotch) {
         return;
       } else {
         collection = scotchId;
       }
       break;
-    case "pale":
+    case 'pale':
       if(pale) {
         return;
       } else {
         collection = paleId;
       }
       break;
-    case "wheat":
+    case 'wheat':
       if(wheat) {
         return;
       } else {
         collection = wheatId;
       }
       break;
-    case "belgian":
+    case 'belgian':
       if(belgian) {
         return;
       } else {
         collection = belgianId;
       }
       break;
-    case "sour":
+    case 'sour':
       if(sour) {
         return;
       } else {
         collection = sourId;
       }
       break;
-    case "bock":
+    case 'bock':
       if(bock) {
         return;
       } else {
         collection = bockId;
       }
       break;
-    case "misc":
+    case 'misc':
       if(misc) {
         return;
       } else {
@@ -121,53 +120,53 @@ var beerHistory = [];
 
   $.ajax({
     url: db + collection + apiKey,
-    type:'GET',
+    type: 'GET',
     async: true,
     contentType: contentType,
     dataType: dataType,
-    success: console.log("Connected to MongoDB")
+    success: console.log('Connected to MongoDB')
   })
   .done(function(response) {
-    // console.log("in done");
+    // console.log('in done');
     switch(style) {
-      case "ipa":
-        // console.log("ipa bitches!");
+      case 'ipa':
+        // console.log('ipa bitches!');
         ipa = response.data;
         // console.log(ipa);
         break;
-      case "strongAle":
+      case 'strongAle':
         strongAle = response.data;
         break;
-      case "stoutPorter":
+      case 'stoutPorter':
         stoutPorter = response.data;
         break;
-      case "lagerPilsner":
+      case 'lagerPilsner':
         lagerPilsner = response.data;
         break;
-      case "scotch":
+      case 'scotch':
         scotch = response.data;
         break;
-      case "pale":
+      case 'pale':
         pale = response.data;
         break;
-      case "wheat":
+      case 'wheat':
         wheat = response.data;
         break;
-      case "belgian":
+      case 'belgian':
         belgian = response.data;
         break;
-      case "sour":
+      case 'sour':
         sour = response.data;
         break;
-      case "bock":
+      case 'bock':
         bock = response.data;
         break;
-      case "misc":
+      case 'misc':
         misc = response.data;
         break;
     }
     // $(document).ajaxStop(function () {
-    //   // $("#loading").hide();
+    //   // $('#loading').hide();
     // });
   })
   .fail(function(error) {
@@ -175,75 +174,65 @@ var beerHistory = [];
   });
 };
 
-// var triedHistoryArray = [];  // THIS HAS TO BE THE BEERS ACUTALLY TRIED, NOT JUST SUGGESTED!
-
-// var totalHistoryArray = [] // this includes suggested beers that have not yet been tried IN ADDITION to those in triedHistoryArray
-
-// requestBeersByStyle("ipa");
 var loopCounter = 0;
 var getRandomBeerByStyle = function(style){
 
-// requestBeersByStyle(style);
-
   var workingArray;
     switch(style) {
-      case "ipa":
-        // console.log("ipa bitches!");
+      case 'ipa':
         workingArray = ipa;
-        // console.log(ipa);
         break;
-      case "strongAle":
+      case 'strongAle':
         workingArray = strongAle;
         break;
-      case "stoutPorter":
+      case 'stoutPorter':
         workingArray = stoutPorter;
         break;
-      case "lagerPilsner":
+      case 'lagerPilsner':
         workingArray = lagerPilsner;
         break;
-      case "scotch":
+      case 'scotch':
         workingArray = scotch;
         break;
-      case "pale":
+      case 'pale':
         workingArray = pale;
         break;
-      case "wheat":
+      case 'wheat':
         workingArray = wheat;
         break;
-      case "belgian":
+      case 'belgian':
         workingArray = belgian;
         break;
-      case "sour":
+      case 'sour':
         workingArray = sour;
         break;
-      case "bock":
+      case 'bock':
         workingArray = bock;
         break;
-      case "misc":
+      case 'misc':
         workingArray = misc;
         break;
     }
-  console.log(workingArray);
 
   var randomBeerIndex = Math.floor(Math.random() * (workingArray.length));
-  if(!localStorage["beerHistory"]) {
-    localStorage["beerHistory"] = JSON.stringify([]);
-    beerHistory = localStorage["beerHistory"];
+  if(!localStorage.beerHistory) {
+    localStorage.beerHistory = JSON.stringify([]);
+    beerHistory = localStorage.beerHistory;
   }
   beerHistory = JSON.parse(localStorage.beerHistory);
 
   //Loop to prevent duplicates
   for (var i = 0; i < beerHistory.length; i++) {
-    if(beerHistory[i].nameDisplay == workingArray[randomBeerIndex].nameDisplay) {
+    if(beerHistory[i].nameDisplay === workingArray[randomBeerIndex].nameDisplay) {
       console.log('The beers were the same!');
       loopCounter++;
       if(loopCounter >= 100) {
-        alert("You've explored all the beers from this style! It will appear in your history twice. Please pick a different style.");
+        alert('You\'ve explored all the beers from this style! It will appear in your history twice. Please pick a different style.');
         break;
       }
       return getRandomBeerByStyle(style);
     }
-  };
+  }
   beerHistory.push(workingArray[randomBeerIndex]);
   localStorage.beerHistory = JSON.stringify(beerHistory);
 
@@ -253,42 +242,42 @@ var getRandomBeerByStyle = function(style){
 //Event Listener randomBeer.html buttons//
   $('.randBtn').on('click', function(e){
     $(document).ajaxStart(function() {
-      $("#loading").fadeIn(1000);
+      $('#loading').fadeIn(1000);
     });
     requestBeersByStyle(e.target.id);
     setTimeout(function() {
       var beer = getRandomBeerByStyle(e.target.id);
       var beerId = beer.id;
-      // $("#loading").hide();
+      // $('#loading').hide();
       window.open('beer.html' + '?id=' + beerId + '&style=' + e.target.id, '_self');
-      // $(".pageTitle").html(beer.name);
+      // $('.pageTitle').html(beer.name);
     }, 7000);
   });
 
 
     var getUserInput = function(input) {
     var urlSearch = window.location.search;
-    var split1 = urlSearch.split("id=");
+    var split1 = urlSearch.split('id=');
     split1 = split1[1];
     var split2 = split1.split('&');
-    if (input === "id"){
+    if (input === 'id'){
       return split2[0];
     }
 
     split2 = split2[1];
-    var split3 = split2.split("=");
+    var split3 = split2.split('=');
 
-    if (input === "style"){
-      return split3[1]
+    if (input === 'style'){
+      return split3[1];
     }
   };
 
   //Event Listener for Beer.html show me something similar button//
-  $('#similarBtn').on('click', function(e){
+  $('#similarBtn').on('click', function(){
     $(document).ajaxStart(function() {
-      $("#loading").fadeIn(1000);
+      $('#loading').fadeIn(1000);
     });
-    var beerStyle = getUserInput("style");
+    var beerStyle = getUserInput('style');
     requestBeersByStyle(beerStyle);
     setTimeout(function() {
       var beer = getRandomBeerByStyle(beerStyle);
@@ -298,82 +287,84 @@ var getRandomBeerByStyle = function(style){
     }, 7000);
   });
 
-  //Listener for the browse selector
-  $('.browseTarget').on('change', function(e){
-    requestBeersByStyle(e.target.value);
-    setTimeout(function() {
-      renderBrowseStyle(e.target.value);
-    }, 7000);
-  });
+
 
   //Render an array in a browseable table
   var renderBrowseStyle = function(style) {
     var bucket;
     var listing;
     switch(style) {
-      case "ipa":
+      case 'ipa':
         bucket = ipa;
         sessionStorage.bucket = JSON.stringify(ipa);
         break;
-      case "strongAle":
+      case 'strongAle':
         bucket = strongAle;
         sessionStorage.bucket = JSON.stringify(strongAle);
         break;
-      case "stoutPorter":
+      case 'stoutPorter':
         bucket = stoutPorter;
         sessionStorage.bucket = JSON.stringify(stoutPorter);
         break;
-      case "lagerPilsner":
+      case 'lagerPilsner':
         bucket = lagerPilsner;
         sessionStorage.bucket = JSON.stringify(lagerPilsner);
         break;
-      case "scotch":
+      case 'scotch':
         bucket = scotch;
         sessionStorage.bucket = JSON.stringify(scotch);
         break;
-      case "pale":
+      case 'pale':
         bucket = pale;
         sessionStorage.bucket = JSON.stringify(pale);
         break;
-      case "wheat":
+      case 'wheat':
         bucket = wheat;
         sessionStorage.bucket = JSON.stringify(wheat);
         break;
-      case "belgian":
+      case 'belgian':
         bucket = belgian;
         sessionStorage.bucket = JSON.stringify(belgian);
         break;
-      case "sour":
+      case 'sour':
         bucket = sour;
         sessionStorage.bucket = JSON.stringify(sour);
         break;
-      case "bock":
+      case 'bock':
         bucket = bock;
         sessionStorage.bucket = JSON.stringify(bock);
         break;
-      case "misc":
+      case 'misc':
         bucket = misc;
         sessionStorage.bucket = JSON.stringify(misc);
         break;
     }
-    $(".browseTable").empty();
-    $(".browseTable").append('<thead><tr class="browseTH"><th><h3>Beer Name</h3></th><th><h3>Brewery</h3></th><th><h3>Beer Style</h3></th><th class="abvTH"><h3>ABV</h3></th><th class="ibuTH"><h3>IBU</h3></th></tr></thead>');
-    console.dir(bucket);
+    $('.browseTable').empty();
+    $('.browseTable').append('<thead><tr class=\'browseTH\'><th><h3>Beer Name</h3></th><th><h3>Brewery</h3></th><th><h3>Beer Style</h3></th><th class=\'abvTH\'><h3>ABV</h3></th><th class=\'ibuTH\'><h3>IBU</h3></th></tr></thead>');
+
     bucket.forEach(function(beer) {
-      beerABV = beer.abv;
-      beerIBU = beer.ibu;
+      var beerABV = beer.abv;
+      var beerIBU = beer.ibu;
       if(!beerABV) {
-        beerABV = "???";
+        beerABV = '???';
       }
       if(!beerIBU) {
-        beerIBU = "???";
+        beerIBU = '???';
       }
-      beerLink = '<a href="./beer.html?id=' + beer.id + '&style=' + style + '">' + beer.name + "</a>";
-      console.log(beerLink);
-      listing = '<tr><td class="name">' + beerLink + "</td>" + "<td>" + beer.breweries[0].name + "</td>" + "<td>" + beer.style.shortName + "</td>" + "<td>" + beerABV + "</td>" + "<td>" + beerIBU + "</td>" + "</tr>";
-      console.log(listing);
-      $(".browseTable").append(listing);
+      var beerLink = '<a href=\'./beer.html?id=' + beer.id + '&style=' + style + '>' + beer.name + '</a>';
+
+      listing = '<tr><td class=\'name\'>' + beerLink + '</td>' + '<td>' + beer.breweries[0].name + '</td>' + '<td>' + beer.style.shortName + '</td>' + '<td>' + beerABV + '</td>' + '<td>' + beerIBU + '</td>' + '</tr>';
+
+      $('.browseTable').append(listing);
     });
-  }
+  };
+
+ //Listener for the browse selector
+  $('.browseTarget').on('change', function(e){
+    requestBeersByStyle(e.target.value);
+    setTimeout(function() {
+      renderBrowseStyle(e.target.value);
+    }, 7000);
+  });
 
 });
