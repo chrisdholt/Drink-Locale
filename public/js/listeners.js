0bbb3b3a3765f81e85080050798be66a3075307e
Chris Holt <christopher.d.holt@gmail.com>
2016-01-20 15:44:09 -0800

    initial sass refactor complete

--- public/js/listeners.js
+++ public/js/listeners.js
@@ -2,9 +2,10 @@
 //<![CDATA[
 $(document).ready(function(){
     $(window).load(function() { // makes sure the whole site is loaded
-      $('#status').fadeOut(); // will first fade out the loading animation
-      $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-      $('body').delay(350).css({'overflow': 'visible'});
+      // $('#status').fadeOut(); // will first fade out the loading animation
+      // $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+      // $('body').delay(350).css({'display': 'visible'});
+      // $('body[class^="main-"]').fadeIn(500);
     });
   //]]>
 });
